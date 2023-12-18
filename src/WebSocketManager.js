c8874a85e3be9adfe79098b3c97d4cb234ed4af4
Lithin87 <72386223+Lithin87@users.noreply.github.com>
2023-12-18 16:15:28 +0530

    removed receive ability

--- src/WebSocketManager.js
+++ src/WebSocketManager.js
@@ -15,12 +15,6 @@ class WebSocketManager {
   setupConnectionHandler() {
     this.io.on('connection', (socket) => {
       console.log('WebSocket client connected : '+socket.id);
-
-      socket.on('Client', (data,ack) => 
-      {
-        console.log('Client:'+socket.id, data.message);
-        ack({ message: 'Message received by the server.' });
-      });
     });
   }
 
